ca1c7d5833efe40cfec90b95b7eed5c4e09041b4
ivand <ivand@appturn.com>
2014-07-01 11:33:55 +0300

    Planets scene with simple rotation scripts

---
+++ Assets/Sources/Orbiter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbiter : MonoBehaviour
+{
+	public GameObject Center = null;
+	public float Speed = 0f;
+
+	void Start()
+	{
+		if (null == Center)
+		{
+			Debug.LogError("Can not work without Center", this);
+			enabled = false;
+		}
+	}
+
+	void Update()
+	{
+		gameObject.transform.RotateAround(Center.transform.position, Vector3.up, Speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Sources/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+	public Vector3 RotationSpeed = Vector3.zero;
+
+	void Update()
+	{
+		gameObject.transform.Rotate(RotationSpeed * Time.deltaTime);
+	}
+}
