f31296fd7c3437c22b97774916a5a93a8d6195fd
ivand <ivand@appturn.com>
2014-07-02 09:44:04 +0300

    Simple starship and stars are flying on the background

---
+++ Assets/Sources/Star.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour 
+{	
+	public float StarSpeed = 20f;
+
+	[System.NonSerialized]
+	public GameController GameController;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 position = transform.position;
+		position.x -= StarSpeed * Time.deltaTime * GameController.SpaceShipSpeed;
+		transform.position = position;
+
+		if (transform.position.x < -20.0f)
+		{
+			Object.Destroy(gameObject);
+		}
+	}
+}
