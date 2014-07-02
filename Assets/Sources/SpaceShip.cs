4f1932abf66f6104af3a0ba5576b4596665079ea
ivand <ivand@appturn.com>
2014-07-02 21:40:48 +0300

    Basic ship movement + some refactoring

---
+++ Assets/Sources/SpaceShip.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceShip : MonoBehaviour, ISpeedProvider
+{
+	#region ISpeedProvider implementation
+
+	public float Speed
+	{
+		get { return Velocity.x; }
+	}
+
+	#endregion
+
+	public Vector2 Velocity = Vector2.right;
+	private Vector2 acceleration = Vector2.zero;
+
+	void Start ()
+	{
+	}
+	
+	void Update ()
+	{
+		#region Input processing
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			acceleration.y += 1f * Time.deltaTime;
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			acceleration.y -= 1f * Time.deltaTime;
+		}
+		else
+		{
+			acceleration.y = -Velocity.y * Time.deltaTime;
+		}
+		#endregion
+
+		Velocity += acceleration;
+		transform.position += new Vector3(0f, Time.deltaTime * Velocity.y, 0f);
+		const float scaleFactor = 30f;
+		transform.localScale = new Vector3(1f, scaleFactor / (scaleFactor + Mathf.Abs (Velocity.y)), 1f);
+	}
+}
---
+++ Assets/Sources/StarSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarSpawner : MonoBehaviour 
+{
+	public GameObject StarPrefab;
+	public float StarAppearingFrequency = 1.0f;
+	public Vector2 VerticalRange = new Vector2(-5, 7);
+	public float StarFlyingDistance = 10f;
+
+	public SpaceShip SpaceShipSpeedProvider;
+
+	private float starTimer;
+
+	void Start () 
+	{
+		starTimer = StarAppearingFrequency;
+	}
+	
+	void Update () 
+	{
+		starTimer -= Time.deltaTime;
+
+		if (starTimer <= 0.0f)
+		{
+			this.GenerateStar();
+			starTimer = StarAppearingFrequency;
+		}
+	}
+
+	public void GenerateStar()
+	{
+		GameObject newStarObj = (GameObject)Object.Instantiate(StarPrefab);
+		Star newStar = newStarObj.GetComponent<Star>();
+		newStar.SpeedProvider = SpaceShipSpeedProvider;
+		newStar.FlyingDistance = StarFlyingDistance;
+
+		float randomY = Random.Range(VerticalRange.x, VerticalRange.y);
+
+		newStarObj.transform.position = new Vector3(transform.position.x, randomY, transform.position.z);
+	}
+}
