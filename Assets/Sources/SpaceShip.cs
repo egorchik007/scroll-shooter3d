4221034f1f0bbbd4239a93ef0b05985fb7b627e6
ivand <ivand@appturn.com>
2014-07-03 07:02:38 +0300

    Shoot simple bullets

--- Assets/Sources/SpaceShip.cs
+++ Assets/Sources/SpaceShip.cs
@@ -17,6 +17,8 @@ public class SpaceShip : MonoBehaviour, ISpeedProvider
 
 	public float RollScaleFactor = 2f;
 
+	public ScreenBounding FieldBounds;
+
 	void Start ()
 	{
 	}
@@ -26,20 +28,75 @@ public class SpaceShip : MonoBehaviour, ISpeedProvider
 		#region Input processing
 		if (Input.GetKey(KeyCode.UpArrow))
 		{
-			acceleration.y += 1f * Time.deltaTime;
+			acceleration.y = 5f * Time.deltaTime;
 		}
 		else if (Input.GetKey(KeyCode.DownArrow))
 		{
-			acceleration.y -= 1f * Time.deltaTime;
+			acceleration.y = -5f * Time.deltaTime;
 		}
 		else
 		{
 			acceleration.y = -Velocity.y * Time.deltaTime;
 		}
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			Move(2f * Time.deltaTime, 0f);
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			Move(-2f * Time.deltaTime, 0f);
+		}
+
+		if (Input.GetKey(KeyCode.Space))
+		{
+			Shoot();
+		}
 		#endregion
 
 		Velocity += acceleration;
-		transform.position += new Vector3(0f, Time.deltaTime * Velocity.y, 0f);
+		Velocity.x = Mathf.Clamp(Velocity.x, 0.1f, 10f);
+		Move(0f, Time.deltaTime * Velocity.y);
 		transform.localRotation = Quaternion.Euler(Velocity.y * RollScaleFactor, 0f, 0f);
+	}
+
+	private void Move(float x, float y)
+	{
+		Vector3 newPosition = transform.position + new Vector3(x, y, 0f);
+		if (FieldBounds.Bounds.Contains(newPosition))
+		{
+			transform.position = newPosition;
+		}
+		else if (0 != y)
+		{
+			Velocity.y = -Velocity.y;
+		}
+	}
+
+	internal class BulletSpeedProvider : ISpeedProvider
+	{
+		private float speed;
+		ISpeedProvider worldSpeedProvider;
+		public BulletSpeedProvider(float speed, ISpeedProvider worldSpeed)
+		{
+			this.speed = speed;
+			this.worldSpeedProvider = worldSpeed;
+		}
+
+		#region ISpeedProvider implementation
+
+		public float Speed
+		{
+			get { return speed - worldSpeedProvider.Speed; }
+		}
+
+		#endregion
+	}
+
+	public GameObject Bullet;
+	private void Shoot()
+	{
+		GameObject newBullet = (GameObject)Object.Instantiate(Bullet, transform.position, Quaternion.identity);
+		newBullet.GetComponent<Star>().SpeedProvider = new BulletSpeedProvider(10.5f, this);
 	}
 }
